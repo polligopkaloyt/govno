6f8121d2543e11bc93d7ce05126f954481ef4558
polligopkaloyt <105598098+polligopkaloyt@users.noreply.github.com>
2025-03-18 10:35:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import os
+import threading
 
 TOKEN = os.getenv("TOKEN")
 bot = telebot.TeleBot(TOKEN)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "бот запущен"
+
+@app.route(f"/{TOKEN}", methods=["POST"])
+def webhook():
+    json_str = request.get_data().decode("UTF-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "!", 200
 
 @bot.message_handler(commands=["start"])
 def start(message):
@@ -30,16 +44,11 @@ def start(message):
 def info_callback(call):
     bot.send_message(call.message.chat.id, "by: @HELLKYXX")
 
-bot.remove_webhook()
-bot.polling()
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "бот запущен"
+def start_bot():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://govno-3gfn.onrender.com/" + TOKEN)
+    bot.infinity_polling()
 
 if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url="https://govno-3gfn.onrender.com/")
+    threading.Thread(target=start_bot, daemon=True).start()
     app.run(host="0.0.0.0", port=10000)  # Открываем порт 10000
-
