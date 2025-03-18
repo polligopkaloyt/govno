c3fa9c18e4db17317def00a59d8e7aed16adb3ba
polligopkaloyt <105598098+polligopkaloyt@users.noreply.github.com>
2025-03-18 10:14:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import os
-from flask import Flask, request
 
 TOKEN = os.getenv("TOKEN")
-
-if not TOKEN:
-    raise ValueError("Ошибка: Токен не найден в переменных окружения!")
-
 bot = telebot.TeleBot(TOKEN)
-
-app = Flask(__name__)
-
-@app.route(f"/{TOKEN}", methods=["POST"])
-def webhook():
-    json_str = request.get_data().decode("UTF-8")
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return "!", 200
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, "Привет! Бот работает.")
+    markup = InlineKeyboardMarkup()
+    
+    # Кнопка "Играть"
+    play_button = InlineKeyboardButton("🎮 Играть", web_app={"url": "https://polligopkaloyt.github.io/creeper.github.io/"})
+    
+    # Кнопка "Информация"
+    info_button = InlineKeyboardButton("ℹ Информация", callback_data="info")
+    
+    # Кнопка "Поддержка"
+    support_button = InlineKeyboardButton("👥 Поддержка", url="https://t.me/C_T_P_A_XX")
+    
+    # Добавляем кнопки в разметку
+    markup.add(play_button)
+    markup.add(info_button, support_button)
+    
+    # Отправляем сообщение
+    bot.send_message(message.chat.id, "привет! я крипер ^_^ и меня надо тапать!", reply_markup=markup)
 
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=f"https://govno-3gfn.onrender.com/{TOKEN}")
-    app.run(host="0.0.0.0", port=5000)
+@bot.callback_query_handler(func=lambda call: call.data == "info")
+def info_callback(call):
+    bot.send_message(call.message.chat.id, "by: @HELLKYXX")
 
+bot.polling()
